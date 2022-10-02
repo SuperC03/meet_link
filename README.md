2dffe23141c39666a421fe60e75fa6193d11795a
Colin C <me@thecolin.co>
2022-10-02 06:26:16 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# meet_link
+
+A blockchain app based on SONR to encourage people to meet up and link.
---
+++ android/app/src/main/kotlin/co/thecolin/meet_link/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.thecolin.meet_link
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
